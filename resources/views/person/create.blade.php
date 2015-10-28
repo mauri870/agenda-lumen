095ae24e85dab1d12a4b891a42af6bf1424c97e7
Mauri de Souza Nunes <mauri870@gmail.com>
2015-10-28 21:51:37 -0200

    Created view for create persons

--- resources/views/person/create.blade.php
+++ resources/views/person/create.blade.php
@@ -1,7 +1,17 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mauri870
- * Date: 28/10/15
- * Time: 20:59
- */
+@extends('layouts.master')
+
+@section('title','Criar Pessoa')
+
+@section('content')
+    <h3>Criar Pessoa</h3>
+    <div class="row text-center">
+        <div class="col-md-6 text-left">
+            {!! Form::open(['url'=>'create/person/']) !!}
+            @include('person.form')
+            <button class="btn btn-success">Cadastrar</button>
+        </div>
+        <div class="col-md-3">
+        </div>
+        {!! Form::close() !!}
+    </div>
+@stop
